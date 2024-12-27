d32cad6f408784555899b549122c5a9e39f1eaa1
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2024-12-27 14:15:09 +0100

    naprawa styli

---
+++ src/app/components/comments-section/comments-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-section',
+  imports: [],
+  templateUrl: './comments-section.component.html',
+  styleUrl: './comments-section.component.css'
+})
+export class CommentsSectionComponent {
+
+}
