bfab349ca43c5225607719e12d736dfa5ce752aa
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2025-01-31 08:47:50 +0100

    small fix

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -15,20 +15,19 @@ export class BlogItemDetailsComponent implements OnInit {
  public image: string = '';
  public text: string = '';
 
- constructor(private service: DataService, private route: ActivatedRoute) {
- }
+  constructor(private service: DataService, private route: ActivatedRoute) {}
 
- ngOnInit() {
-   let id: string = '';
-   this.route.paramMap
-     .subscribe((params: any) => {
-        id = params.get('id');
-     });
+  ngOnInit() {
+    let id: string = '';
+    this.route.paramMap
+      .subscribe((params: any) => {
+          id = params.get('id');
+      });
 
-   this.service.getById(id).subscribe((res: any) => {
-      this.image = res[0].image;
-      this.text = res[0].text;
-   });
+    this.service.getById(id).subscribe((res: any) => {
+        this.image = res[0].image;
+        this.text = res[0].text;
+    });
 
- }
+  }
 }
